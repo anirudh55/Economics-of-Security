e99268f60969e960f4f84f0304e2a8815b52639c
Lisa de Wilde <lisadewilde92@gmail.com>
2015-10-12 11:05:56 +0200

    Layout mappen aangepast

--- Code/Block 2/detectedpermonth.R
+++ Code/Block 2/detectedpermonth.R
@@ -1,10 +1,11 @@
-ZeusTracker <- read.csv("~/Economics-of-Security/Data/ZeusTracker_20150915.csv")
-FeodoTracker <- read.csv("~/Economics-of-Security/Data/FeodoTracker_20150915.csv")
+ZeusTracker <- read.csv("~/Documents/Master/ZeusTracker_20150915.csv")
+FeodoTracker <- read.csv("~/Documents/Master/FeodoTracker_20150915.csv")
 
 dates_zeus <- strftime(ZeusTracker$Dateadded, "%Y/%m")
 dates_zeus <- table(dates_zeus)
 
 barplot(dates_zeus, col=rainbow(1))
+
 
 dates <- strptime(FeodoTracker$First.seen..UTC.,"%m/%d/%Y %H:%M")
 dates_feodo <- strftime(dates, "%Y/%m")
